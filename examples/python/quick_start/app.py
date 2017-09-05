9ca655edd5b03fadcb72fd976df6d77f9af8d277
stefan <stefan.bergmann.cx@gmail.com>
2017-09-05 14:52:40 +0200

    change quickstart

--- examples/python/quick_start/app.py
+++ examples/python/quick_start/app.py
@@ -2,17 +2,11 @@ from e2c import E2c
 
 config = (
     '.run -- action',
-    'action.out -- print',
     'action.out -- print')
 
-def print_data(x: int, data):
-    print(x, data)
-
-
-
 e2c = E2c[str, str](config)
-e2c.actor('action', lambda data, out: out(data, {'a': 1}))
-e2c.actor('print', print_data)
+e2c.actor('action', lambda data, out: out(data))
+e2c.actor('print', lambda data: print(data))
 
 e2c.visualize()
 e2c.run('hello')
